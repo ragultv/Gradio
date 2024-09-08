8c4e6418f57142d3be387985378c947e35d1bbb9
ragul <141637129+ragultv@users.noreply.github.com>
2024-09-08 21:59:34 +0530

    Update license-plate-detection.py

--- license-plate-detection.py
+++ license-plate-detection.py
@@ -1,1 +1,13 @@
+import gradio as gr
+from ultralytics import YOLO
 
+#load the model
+model=YOLO('best.pt')
+#define the detectiom
+def detect(img):
+    results=model(img)
+    return results[0].plot()
+
+iface=gr.Interface(fn=detect,inputs='image',outputs='image',)
+#launch the interface
+iface.launch()
